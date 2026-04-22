71a6bcf1da262f2614c8bb56fe1b6929af380b2d
Zhao <yiming.zhao@cotiviti.com>
2026-04-22 17:43:20 -0400

    Fix bundled reference filename: CV_OA -> CCV_OA

--- CCV_OA_Mapping_app/CCV_OA_Mapping_app.py
+++ CCV_OA_Mapping_app/CCV_OA_Mapping_app.py
@@ -17,7 +17,7 @@ import streamlit as st
 # ---------------------------------------------------------------------------
 RAW_COL = "Client Data Fields"
 TARGET_COL = "Cotiviti Data Fields"
-BUNDLED_REF = os.path.join(os.path.dirname(__file__), "CV_OA_Mapping_reference.xlsx")
+BUNDLED_REF = os.path.join(os.path.dirname(__file__), "CCV_OA_Mapping_reference.xlsx")
 
 # ---------------------------------------------------------------------------
 # Text normalisation
